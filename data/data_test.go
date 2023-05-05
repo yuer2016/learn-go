de8d979ef80bee6fb846c327568184497cb2e3bc
yuer2021 <983312113@qq.com>
2023-05-06 00:27:13 +0800

    add method test

--- data/data_test.go
+++ data/data_test.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"unsafe"
 )
@@ -33,8 +34,10 @@ func TestString(t *testing.T) {
 	println(s3, s4, s5)
 }
 
-/*定义数组类型时,数组长度必须是非负整型常量表达式,长度是类型组成部分。
-元素类型相同,但长度不同的数组不属于同一类型*/
+/*
+定义数组类型时,数组长度必须是非负整型常量表达式,长度是类型组成部分。
+元素类型相同,但长度不同的数组不属于同一类型
+*/
 func TestArray(t *testing.T) {
 	var a [4]int // 元素自动初始化为零
 
@@ -137,8 +140,10 @@ func TestSlice(t *testing.T) {
 	}
 }
 
-/*可直接创建切片对象,无须预先准备数组。
-因为是引用类型,须使用 make 函数或显式初始化语句,它会自动完成底层数组内存分配*/
+/*
+可直接创建切片对象,无须预先准备数组。
+因为是引用类型,须使用 make 函数或显式初始化语句,它会自动完成底层数组内存分配
+*/
 func TestInitSlice(t *testing.T) {
 	s1 := make([]int, 3, 5)    // 指定 len、cap,底层数组初始化为零值
 	s2 := make([]int, 3)       // 省略 cap, 和 len 相等
@@ -165,7 +170,7 @@ func TestCopySlice(t *testing.T) {
 字典(哈希表)是一种使用频率极高的数据结构。
 将其作为语言内置类型，从运行时层面进行优化,可获得更高效的性能。
 作为无序键值对集合,字典要求 key 必须是支持相等运算符（==、!=）的数据类型,比如,数字、字符串、指针、数组、结构体,以及对应接口类型。
-字典是引用类型，使用 make 函数或初始化表达语句来创建
+字典是引用类型,使用 make 函数或初始化表达语句来创建.
 */
 func TestMap(t *testing.T) {
 	m := make(map[string]int)
@@ -193,6 +198,8 @@ func TestMap(t *testing.T) {
 
 	//删除键值对。不存在时，不会报错
 	delete(m, "d")
+	//函数 len 返回当前键值对数量, cap 不接受字典类型.
+	println(len(m))
 }
 
 /*字典进行迭代，每次返回的键值次序都不相同*/
@@ -209,4 +216,241 @@ func TestMapRange(t *testing.T) {
 		}
 		println()
 	}
+}
+
+/*
+在迭代期间删除或新增键值是安全的.
+运行时会对字典并发操作做出检测。
+如果某个任务正在对字典进行写操作,那么其他任务就不能对该字典执行并发操作(读、写、删除),否则会导致进程崩溃
+*/
+func TestIteration(t *testing.T) {
+	m := make(map[int]int)
+
+	for i := 0; i < 10; i++ {
+		m[i] = i + 10
+	}
+
+	for k := range m {
+		if k == 5 {
+			m[100] = 1000
+		}
+
+		delete(m, k)
+		fmt.Println(k, m)
+	}
+}
+
+/*
+结构体 (struct) 将多个不同类型命名字段 (field) 序列打包成一个复合类型。
+字段名必须唯一，可用 “_” 补位，支持使用自身指针类型成员。
+字段名、排列顺序属类型组成部分。
+除对齐处理外，编译器不会优化、调整内存布局。
+*/
+type node struct {
+	_    int
+	id   int
+	next *node
+}
+
+func TestStruct(t *testing.T) {
+	n1 := node{
+		id: 1,
+	}
+
+	n2 := node{
+		id:   2,
+		next: &n1,
+	}
+
+	fmt.Println(n1, n2)
+
+	/*可按顺序初始化全部字段*/
+	type user struct {
+		name string
+		age  byte
+	}
+
+	u1 := user{"Tom", 12}
+	fmt.Println(u1)
+
+	/*可直接定义匿名结构类型变量,或用作字段类型。
+	但因其缺少类型标识，在作为字段类型时无法直接初始化*/
+	u := struct { // 直接定义匿名结构变量
+		name string
+		age  byte
+	}{
+		name: "Tom",
+		age:  12,
+	}
+
+	type file struct {
+		name string
+		attr struct { // 定义匿名结构类型字段
+			owner int
+			perm  int
+		}
+	}
+
+	f := file{
+		name: "test.dat",
+	}
+
+	f.attr.owner = 1
+	f.attr.perm = 0755
+
+	fmt.Println(u, f)
+}
+
+/*
+空结构 (struct{}) 是指没有字段的结构类型。
+它比较特殊,因为无论是其自身,还是作为数组元素类型,其长度都为零。
+*/
+func TestEmptyStruct(t *testing.T) {
+	var a struct{}
+	var b [100]struct{}
+
+	println(unsafe.Sizeof(a), unsafe.Sizeof(b))
+
+	/*空结构可作为通道元素类型,用于事件通知*/
+	exit := make(chan struct{})
+
+	go func() {
+		println("hello,world!")
+		exit <- struct{}{}
+	}()
+
+	<-exit
+	println("end.")
+}
+
+/*匿名字段(anonymous field),是指没有名字,仅有类型的字段,也被称作嵌入字段或嵌入类型*/
+func TestAnonymousStructField(t *testing.T) {
+	type attr struct {
+		perm int
+	}
+
+	type file struct {
+		name string
+		attr // 仅有类型名
+	}
+
+	f := file{
+		name: "test.dat",
+		attr: attr{ // 显式初始化匿名字段
+			perm: 0755,
+		},
+	}
+
+	f.perm = 0644   // 直接设置匿名字段成员
+	println(f.perm) // 直接读取匿名字段成员
+
+	/*如果多个相同层级的匿名字段成员重名，就只能使用显式字段名访问，因为编译器无法确定目标*/
+	type files struct {
+		name string
+	}
+
+	type log struct {
+		name string
+	}
+
+	type data struct {
+		files
+		log
+	}
+
+	d := data{}
+	d.files.name = "file name one" // 显式字段名
+	d.log.name = "log name one"
+
+	fmt.Println(d)
+}
+
+/*
+字段标签 (tag) 并不是注释, 而是用来对字段进行描述的元数据。
+尽管它不属于数据成员，但却是类型的组成部分.
+在运行期,可用反射获取标签信息。
+它常被用作格式校验，数据库关系映射等。
+*/
+func TestFieldTags(t *testing.T) {
+	type user struct {
+		name string `昵称`
+		sex  byte   `性别`
+	}
+
+	u := user{"Tom", 1}
+	v := reflect.ValueOf(u)
+	t1 := v.Type()
+
+	for i, n := 0, t1.NumField(); i < n; i++ {
+		fmt.Printf("%s: %v\n", t1.Field(i).Tag, v.Field(i))
+	}
+}
+
+/*
+内存布局
+不管结构体包含多少字段，其内存总是一次性分配的，各字段在相邻的地址空间按定义顺序排列。
+在分配内存时,字段须做对齐处理,通常以所有字段中最长的基础类型宽度为标准。
+*/
+func TestMemoryStruct(t *testing.T) {
+	type point struct {
+		x, y int
+	}
+
+	type value struct {
+		id    int    // 基本类型
+		name  string // 字符串
+		data  []byte // 引用类型
+		next  *value // 指针类型
+		point        // 匿名字段
+	}
+
+	v := value{
+		id:    1,
+		name:  "test",
+		data:  []byte{1, 2, 3, 4},
+		point: point{x: 100, y: 200},
+	}
+
+	s := ` 
+		v: %p~ %x,size: %d,align: %d
+
+		field address      offset size
+		 ------+--------------+--------+----------- 
+		id     %p            %d      %d
+		name   %p            %d      %d
+		data   %p            %d      %d
+		next   %p            %d      %d
+		x      %p            %d      %d
+		y      %p            %d      %d
+ `
+
+	fmt.Printf(s,
+		&v, uintptr(unsafe.Pointer(&v))+unsafe.Sizeof(v), unsafe.Sizeof(v), unsafe.Alignof(v),
+		&v.id, unsafe.Offsetof(v.id), unsafe.Sizeof(v.id),
+		&v.name, unsafe.Offsetof(v.name), unsafe.Sizeof(v.name),
+		&v.data, unsafe.Offsetof(v.data), unsafe.Sizeof(v.data),
+		&v.next, unsafe.Offsetof(v.next), unsafe.Sizeof(v.next),
+		&v.x, unsafe.Offsetof(v.x), unsafe.Sizeof(v.x),
+		&v.y, unsafe.Offsetof(v.y), unsafe.Sizeof(v.y))
+
+	v1 := struct {
+		a byte
+		b byte
+		c int32 // 对齐宽度4
+	}{}
+
+	v2 := struct {
+		a byte
+		b byte // 对齐宽度1
+	}{}
+
+	v3 := struct {
+		a byte
+		b []int // 基础类型int，对齐宽度8
+		c byte
+	}{}
+
+	fmt.Printf("v1: %d, %d\n", unsafe.Alignof(v1), unsafe.Sizeof(v1))
+	fmt.Printf("v2: %d, %d\n", unsafe.Alignof(v2), unsafe.Sizeof(v2))
+	fmt.Printf("v3: %d, %d\n", unsafe.Alignof(v3), unsafe.Sizeof(v3))
 }
--- method/method_test.go
+++ method/method_test.go
@@ -1,1 +1,52 @@
 package method
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*方法是与对象实例绑定的特殊函数。方法是有关联状态的，而函数通常没有。*/
+//可以为当前包,以及除接口和指针以外的任何类型定义方法,方法同样不支持重载(overload)
+type N int
+
+func (n N) toString() string {
+	return fmt.Sprintf("%#x", n)
+}
+
+func (n N) value() {
+	n++
+	fmt.Printf("v: %p, %v\n", &n, n)
+}
+
+func (n *N) pointer() {
+	(*n)++
+	fmt.Printf("p: %p, %v\n", n, *n)
+}
+
+func TestMethodDefine(t *testing.T) {
+	var a N = 25
+	p := &a
+	println(a.toString())
+
+	println()
+
+	a.value()
+	a.pointer()
+
+	println()
+
+	p.value()
+	p.pointer()
+
+	println()
+	fmt.Printf("a: %p, %v\n", &a, a)
+}
+
+/*
+如何选择方法的 receiver 类型？
+要修改实例状态,用 *T
+无须修改状态的小对象或固定值，建议用 T
+大对象建议用 *T, 以减少复制成本
+引用类型、字符串、函数等指针包装对象，直接用 T
+若包含 Mutex 等同步字段, 用 *T，避免因复制造成锁操作无效
+其他无法确定的情况, 都用 *T*/
